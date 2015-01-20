d0c26c5b7318c82c8fc05ece86fd43e5bacff93f
MetaSean <metasean@users.noreply.github.com>
2015-01-20 06:35:59 -0700

    fixed onclick function call; added console.log messages

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,17 +1,21 @@
-var camera = {
+var customCamera = {
 
-	takeShot: function() {navigator.camera.getPicture(onSuccess, onFail, {
-		quality        : 50,
-		destinationType: Camera.DestinationType.FILE_URI
-	})},
+	takeShot: function() {
+		console.log('takeShot called')
+		navigator.camera.getPicture(onSuccess, onFail, {
+			quality        : 50,
+			destinationType: Camera.DestinationType.FILE_URI
+		})
+	},
 
 	onSuccess: function(imageURI) {
 		var image = document.getElementById('myImage');
 		image.src = imageURI;
+		console.log('Success.  image.src: ' + image.src);
 	},
 
 	onFail: function(message) {
-		alert('Failed because: ' + message);
+		console.log('Failed because: ' + message);
 	}
 
 };
