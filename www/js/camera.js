fcc45424b1c79ec73bee796d44029eeeb6e26daa
MetaSean <metasean@users.noreply.github.com>
2015-01-20 13:52:40 -0700

    removed non-working example 1

--- www/js/camera.js
+++ www/js/camera.js
@@ -1,25 +1,3 @@
-var customCamera = {
-
-	takeShot: function() {
-		alert('takeShot called');
-		navigator.camera.getPicture(onSuccess, onFail, {
-			quality        : 50,
-			destinationType: Camera.DestinationType.FILE_URI
-		})
-	},
-
-	onSuccess: function(imageURI) {
-		var image = document.getElementById('myImage');
-		image.src = imageURI;
-		alert('Success.  image.src: ' + image.src);
-	},
-
-	onFail: function(message) {
-		alert('Failed because: ' + message);
-	}
-
-};
-
 function capturePhoto(){
 	navigator.camera.getPicture(photoTaken, error,
 		{
